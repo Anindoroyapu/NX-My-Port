3dc9d616f30ab76524551298ad28830e32b0c8ff
Anindo Roy <anindoroy112@gmail.com>
2025-10-15 12:03:00 +0600

    feat: Add HeaderOne component to Page for improved layout structure

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,12 +1,20 @@
 import PostsPage from "@/components/posts/PostsPage";
+import HeaderOne from "@/layouts/headers/HeaderOne";
 import Wrapper from "@/layouts/Wrapper";
 import React from "react";
 
 const Page = () => {
   return (
     <Wrapper>
-      <div className=" bg-white">
-        <PostsPage />
+      <HeaderOne />
+      <div
+        className="bg-white"
+        id="smooth-wrapper"
+        style={{ paddingTop: "100px" }}
+      >
+        <div id="smooth-content">
+          <PostsPage />
+        </div>
       </div>
     </Wrapper>
   );
